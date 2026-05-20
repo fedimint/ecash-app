a4f931d49b43fd3a54abef4ae2534d29d69b7200
Nathan Moeller <n.moeller18@gmail.com>
2026-05-19 20:11:31 -0700

    Dart formatting

--- lib/error_helper.dart
+++ lib/error_helper.dart
@@ -9,7 +9,8 @@ import 'package:flutter/material.dart';
 /// Unknown errors fall through to the existing generic [context.l10n.failedToSendPayment]
 /// so we never surface a stack trace to the user.
 String ecashAppErrorToL10n(BuildContext context, Object err) {
-  if (err is EcashAppError_ExpiredInvoice) return context.l10n.errExpiredInvoice;
+  if (err is EcashAppError_ExpiredInvoice)
+    return context.l10n.errExpiredInvoice;
   if (err is EcashAppError_InsufficientBalance) {
     return context.l10n.errInsufficientBalance(
       err.neededMsats.toInt(),
@@ -17,16 +18,20 @@ String ecashAppErrorToL10n(BuildContext context, Object err) {
     );
   }
   if (err is EcashAppError_NoRouteFound) return context.l10n.errNoRouteFound;
-  if (err is EcashAppError_GatewayOffline) return context.l10n.errGatewayOffline;
-  if (err is EcashAppError_NoGatewaysAvailable) return context.l10n.errNoGateways;
-  if (err is EcashAppError_FederationOffline) return context.l10n.errFederationOffline;
+  if (err is EcashAppError_GatewayOffline)
+    return context.l10n.errGatewayOffline;
+  if (err is EcashAppError_NoGatewaysAvailable)
+    return context.l10n.errNoGateways;
+  if (err is EcashAppError_FederationOffline)
+    return context.l10n.errFederationOffline;
   if (err is EcashAppError_InvalidInvoice) {
     return context.l10n.errInvalidInvoice(err.field0);
   }
   if (err is EcashAppError_InvalidAddress) {
     return context.l10n.errInvalidAddress(err.field0);
   }
-  if (err is EcashAppError_PaymentRefunded) return context.l10n.errPaymentRefunded;
+  if (err is EcashAppError_PaymentRefunded)
+    return context.l10n.errPaymentRefunded;
   if (err is EcashAppError_Timeout) return context.l10n.errTimeout;
   if (err is EcashAppError_Other) return err.field0;
   return context.l10n.failedToSendPayment;
